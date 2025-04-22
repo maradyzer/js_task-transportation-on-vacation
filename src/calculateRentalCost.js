f5b0ea3d5d88777dfc17b4f1e8737dd7c98affd1
Andrew Pavlenko <andrew.pawllenko@gmail.com>
2025-04-22 14:31:39 +0300

    fix the grammar mistakes

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -6,18 +6,18 @@
 function calculateRentalCost(days) {
   const PRICE_PER_DAY = 40;
   const LONG_TERM = 7;
-  const LONT_TERM_DISCOUNT = 50;
+  const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM = 3;
-  const SHORM_TERM_DISCOUNT = 20;
+  const SHORT_TERM_DISCOUNT = 20;
 
   const basePrice = days * PRICE_PER_DAY;
 
   if (days >= LONG_TERM) {
-    return basePrice - LONT_TERM_DISCOUNT;
+    return basePrice - LONG_TERM_DISCOUNT;
   }
 
   if (days >= SHORT_TERM) {
-    return basePrice - SHORM_TERM_DISCOUNT;
+    return basePrice - SHORT_TERM_DISCOUNT;
   }
 
   return basePrice;
